02466fa5048c7e351e60edbbf22eb24caebd3dcd
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2021-12-27 17:42:01 -0300

    Visual página create user

---
+++ src/pages/users/create.tsx
@@ -0,0 +1,30 @@
+import { Flex, Box, Heading, Divider, VStack, SimpleGrid, HStack, Button } from "@chakra-ui/react";
+import { Input } from "../../components/Form/Input";
+import { Base } from "../../components/Template/Base";
+
+export default function CreateUser() {
+    return (
+        <Base>
+            <Box flex="1" borderRadius="8px" bg="gray.800" p="8">
+                <Heading size="lg" fontWeight="normal">Criar usuário</Heading>
+                <Divider my="6" borderColor="gray.700" />
+                <VStack spacing="8">
+                    <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+                        <Input name="name" label="Nome completo" />
+                        <Input name="email" label="Email" type="email" />
+                    </SimpleGrid>
+                    <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+                        <Input name="password" label="Senha" />
+                        <Input name="password_confirmation" label="Confirme sua senha" type="password" />
+                    </SimpleGrid>
+                </VStack>
+                <Flex mt="8" justify="flex-end">
+                    <HStack spacing="4">
+                        <Button colorScheme="whiteAlpha">Cancelar</Button>
+                        <Button colorScheme="pink">Salvar</Button>
+                    </HStack>
+                </Flex>
+            </Box>
+       </Base>
+    )
+}
