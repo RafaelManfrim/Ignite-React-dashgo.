f8e12a495e2a9aa927fa76ec6591d829417207f6
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2021-12-27 10:51:55 -0300

    Componentizando Input, tela SignIn

---
+++ src/components/Form/Input.tsx
@@ -0,0 +1,15 @@
+import { Input as ChakraInput, FormLabel, FormControl, InputProps as ChakraInputProps } from '@chakra-ui/react'
+
+interface InputProps extends ChakraInputProps {
+    name: string;
+    label?: string;
+} 
+
+export function Input({ name, label, ...rest }: InputProps) {
+    return (
+        <FormControl>
+            { !!label && <FormLabel htmlFor={name}>{label}</FormLabel> }
+            <ChakraInput id={name} name={name} focusBorderColor="pink.500" bgColor="gray.900" variant="filled" _hover={{ bgColor: 'gray.900'}} size="lg" {...rest}/>
+        </FormControl>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,15 @@
-import type { NextPage } from 'next'
-import { Flex, Input, Button, Stack, FormLabel, FormControl } from '@chakra-ui/react'
+import { Input } from '../components/Form/Input'
 
-const Home: NextPage = () => {
+import type { NextPage } from 'next'
+import { Flex, Button, Stack } from '@chakra-ui/react'
+
+const SignIn: NextPage = () => {
 	return (
 		<Flex w="100vw" h="100vh" align="center" justify="center">
 			<Flex as="form" width="100%" maxWidth="360px" bg="gray.800" p="8" borderRadius="8px" flexDir="column">
 				<Stack spacing="4">
-					<FormControl>
-						<FormLabel htmlFor="email">Email: </FormLabel>
-						<Input id="email" name="email" type="email" focusBorderColor="pink.500" bgColor="gray.900" variant="filled" _hover={{ bgColor: 'gray.900'}} size="lg" />
-					</FormControl>
-					<FormControl>
-						<FormLabel htmlFor="password">Senha: </FormLabel>
-						<Input id="password" name="password" type="password" focusBorderColor="pink.500" bgColor="gray.900" variant="filled" _hover={{ bgColor: 'gray.900'}} size="lg" />
-					</FormControl>
+					<Input name="email" label="Email" type="email"/>
+					<Input name="password" label="Password" type="password"/>
 				</Stack>
 				<Button type="submit" mt="6" colorScheme="pink" size="lg">Entrar</Button>
 			</Flex>
@@ -21,4 +17,4 @@ const Home: NextPage = () => {
 	)
 }
 
-export default Home
+export default SignIn
