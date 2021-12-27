49b62e742bf5871f01839bf93a6eea964afa7352
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2021-12-27 09:55:43 -0300

    pagina de login desenvolvida

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,24 @@
 import type { NextPage } from 'next'
+import { Flex, Input, Button, Stack, FormLabel, FormControl } from '@chakra-ui/react'
 
 const Home: NextPage = () => {
-  return (
-    <>
-      
-    </>
-  )
+	return (
+		<Flex w="100vw" h="100vh" align="center" justify="center">
+			<Flex as="form" width="100%" maxWidth="360px" bg="gray.800" p="8" borderRadius="8px" flexDir="column">
+				<Stack spacing="4">
+					<FormControl>
+						<FormLabel htmlFor="email">Email: </FormLabel>
+						<Input id="email" name="email" type="email" focusBorderColor="pink.500" bgColor="gray.900" variant="filled" _hover={{ bgColor: 'gray.900'}} size="lg" />
+					</FormControl>
+					<FormControl>
+						<FormLabel htmlFor="password">Senha: </FormLabel>
+						<Input id="password" name="password" type="password" focusBorderColor="pink.500" bgColor="gray.900" variant="filled" _hover={{ bgColor: 'gray.900'}} size="lg" />
+					</FormControl>
+				</Stack>
+				<Button type="submit" mt="6" colorScheme="pink" size="lg">Entrar</Button>
+			</Flex>
+		</Flex>
+	)
 }
 
 export default Home
