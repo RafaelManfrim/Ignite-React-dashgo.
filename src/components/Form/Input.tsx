c980c292839913c622bf93aa825f4e5df564d46a
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2021-12-28 21:02:30 -0300

    Validando dados de autenticação com yup

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -1,16 +1,21 @@
-import { Input as ChakraInput, FormLabel, FormControl, InputProps } from '@chakra-ui/react'
+import { Input as ChakraInput, FormLabel, FormControl, InputProps, FormErrorMessage } from '@chakra-ui/react'
 import { forwardRef, ForwardRefRenderFunction as ForwardRefFunc } from 'react'
+import { FieldError } from 'react-hook-form'
 
 interface InputBaseProps extends InputProps {
     name: string;
     label?: string;
+    error?: FieldError
 } 
 
-const InputBase: ForwardRefFunc<HTMLInputElement, InputBaseProps> = ({ name, label, ...rest }, ref) => {
+const InputBase: ForwardRefFunc<HTMLInputElement, InputBaseProps> = ({ name, label, error, ...rest }, ref) => {
     return (
-        <FormControl>
+        <FormControl isInvalid={!!error}>
             { !!label && <FormLabel htmlFor={name}>{label}</FormLabel> }
             <ChakraInput id={name} name={name} focusBorderColor="pink.500" bgColor="gray.900" variant="filled" _hover={{ bgColor: 'gray.900'}} size="lg" ref={ref} {...rest}/>
+            { !!error && (
+                <FormErrorMessage>{error.message}</FormErrorMessage>
+            )}
         </FormControl>
     )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,36 @@
 import type { NextPage } from 'next'
 import { SubmitHandler, useForm } from 'react-hook-form'
 import { Flex, Button, Stack } from '@chakra-ui/react'
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
 import { Input } from '../components/Form/Input'
+import Router from 'next/router'
 
 type SignInFormData = {
 	email: string
 	password: string
 }
 
+const signInFormSchema = yup.object().shape({
+	email: yup.string().required('Email obrigatório').email(),
+	password: yup.string().required('Senha obrigatória').min(6, 'A senha precisa de no mínimo 6 caracteres')
+})
+
 const SignIn: NextPage = () => {
-	const { register, handleSubmit, formState } = useForm()
+	const { register, handleSubmit, formState } = useForm({
+		resolver: yupResolver(signInFormSchema)
+	})
 
 	const handleSignIn: SubmitHandler<SignInFormData> = (data) => {
-		console.log(data)
+		Router.push('/dashboard')
 	}
  
 	return (
 		<Flex w="100vw" h="100vh" align="center" justify="center">
 			<Flex as="form" width="100%" maxWidth="360px" bg="gray.800" p="8" borderRadius="8px" flexDir="column" onSubmit={handleSubmit(handleSignIn)}>
 				<Stack spacing="4">
-					<Input label="Email" type="email" {...register('email')}/>
-					<Input label="Password" type="password" {...register('password')}/>
+					<Input label="Email" type="email" error={formState.errors.email} {...register('email')}/>
+					<Input label="Senha" type="password" error={formState.errors.password} {...register('password')}/>
 				</Stack>
 				<Button type="submit" mt="6" colorScheme="pink" size="lg" isLoading={formState.isSubmitting}>Entrar</Button>
 			</Flex>
