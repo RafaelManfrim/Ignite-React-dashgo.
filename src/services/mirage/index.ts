c39825122cfc8d8b4193184fda6883f92d799eab
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2021-12-30 10:01:34 -0300

    Configurando mirageJs

---
+++ src/services/mirage/index.ts
@@ -0,0 +1,46 @@
+import { createServer, Factory, Model } from "miragejs"
+import faker from 'faker'
+
+type User = {
+    name: string
+    email: string
+    createdAt: string
+}
+
+export function makeServer() {
+    const server = createServer({
+        models: {
+            user: Model.extend<Partial<User>>({})
+        },
+
+        factories: {
+            user: Factory.extend({
+                name(i) {
+                    return `User ${i + 1}`
+                },
+                email() {
+                    return faker.internet.email().toLowerCase()
+                },
+                createdAt() {
+                    return faker.date.recent(1000)
+                },
+            })
+        },
+
+        seeds(server) {
+            server.createList('user', 150)
+        },
+        
+        routes() {
+            this.namespace = 'mirage'
+            this.timing = 500
+
+            this.get('/users/')
+            this.post('/users/')
+
+            this.passthrough()
+        }
+    })
+
+    return server
+}
